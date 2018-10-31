c0e65576d3045e424ab5200c72e93035e05e744f
mSengera <work@marvin-sengera.de>
2018-10-31 11:16:12 +0100

    Add breadth first search

---
+++ 8-puzzle-problem_breadth-first-search/Node.py
@@ -0,0 +1,103 @@
+import copy
+
+"""
+Node class with basic functions and data structure
+"""
+class Node:
+    boardConfiguration = []
+    nullPosition = []
+    backpointer = ""
+    depth = -1
+
+    def __init__(self, boardConfiguration, nullPointer, depth):
+        self.setBoardConfiguration(boardConfiguration)
+        self.setNullPosition(nullPointer)
+        self.setDepth(depth)
+
+    def successors(self):
+        successors = []
+
+        node = self.getBoardConfiguration()
+        zero_point = self.getNullPosition()
+        depth = self.getDepth() + 1
+
+        # Up
+        successor_node = copy.deepcopy(node)
+        changed_zero_y = zero_point[0] - 1
+
+        if 0 <= changed_zero_y <= 2:
+            successor_node[zero_point[0]][zero_point[1]] = node[changed_zero_y][zero_point[1]]
+            successor_node[changed_zero_y][zero_point[1]] = 0
+
+            new_node = Node(successor_node, [changed_zero_y, zero_point[1]], depth)
+            new_node.setBackpointer(self)
+
+            successors.append(new_node)
+
+        # Down
+        successor_node = copy.deepcopy(node)
+        changed_zero_y = zero_point[0] + 1
+
+        if 0 <= changed_zero_y <= 2:
+            successor_node[zero_point[0]][zero_point[1]] = node[changed_zero_y][zero_point[1]]
+            successor_node[changed_zero_y][zero_point[1]] = 0
+
+            new_node = Node(successor_node, [changed_zero_y, zero_point[1]], depth)
+            new_node.setBackpointer(self)
+
+            successors.append(new_node)
+
+        # Left
+        successor_node = copy.deepcopy(node)
+        changed_zero_x = zero_point[1] - 1
+
+        if 0 <= changed_zero_x <= 2:
+            successor_node[zero_point[0]][zero_point[1]] = node[zero_point[0]][changed_zero_x]
+            successor_node[zero_point[0]][changed_zero_x] = 0
+
+            new_node = Node(successor_node, [changed_zero_y, zero_point[1]], depth)
+            new_node.setBackpointer(self)
+
+            successors.append(new_node)
+
+        # Right
+        successor_node = copy.deepcopy(node)
+        changed_zero_x = zero_point[1] + 1
+
+        if 0 <= changed_zero_x <= 2:
+            successor_node[zero_point[0]][zero_point[1]] = node[zero_point[0]][changed_zero_x]
+            successor_node[zero_point[0]][changed_zero_x] = 0
+
+            new_node = Node(successor_node, [changed_zero_y, zero_point[1]], depth)
+            new_node.setBackpointer(self)
+
+            successors.append(new_node)
+
+        return successors
+
+    """
+    Getter and Setter Methods
+    """
+    def setBoardConfiguration(self, boardConfiguration):
+        self.boardConfiguration = boardConfiguration
+
+    def getBoardConfiguration(self):
+        return self.boardConfiguration
+
+    def setNullPosition(self, nullPosition):
+        self.nullPosition = nullPosition
+
+    def getNullPosition(self):
+        return self.nullPosition
+
+    def setBackpointer(self, backpointer):
+        self.backpointer = backpointer
+
+    def getBackpointer(self):
+        return self.backpointer
+
+    def setDepth(self, depth):
+        self.depth = depth
+
+    def getDepth(self):
+        return self.depth
---
+++ 8-puzzle-problem_breadth-first-search/breadth-first-search.py
@@ -0,0 +1,59 @@
+# Import Node class
+from Node import Node
+
+"""
+Get a solution path from an starting board configuration s, to a goal node.
+"""
+def DFS(s, goal_node, k):
+    OPEN = []
+    CLOSED = []
+
+    # Append S (Starting Node) top OPEN
+    OPEN.append(s)
+
+    if not OPEN:
+        return False
+
+    while OPEN:
+        n = OPEN.pop(0)
+
+        CLOSED.append(n)
+
+        for successor in n.successors():
+            if successor.getBoardConfiguration() == goal_node:
+                return successor
+
+            if n.getDepth() <= k:
+                OPEN.append(successor)
+
+    return False
+
+"""
+Main Programm
+"""
+
+# Startng Point "s"
+starting_board_configuration = \
+        [
+            [1, 2, 3],
+            [0, 8, 4],
+            [7, 6, 5]
+        ]
+
+s = Node(starting_board_configuration, [1, 0], 0)
+
+# target board configuration
+target_board_configuration = \
+        [
+            [1, 2, 3],
+            [8, 0, 4],
+            [7, 6, 5]
+        ]
+
+g = target_board_configuration
+
+
+solution_node = DFS(s, g, 5)
+
+# Further computation from solution_path. With backpointer, reconstruct the solution path
+print(solution_node.getBackpointer())
